824e0e7c88e0453e91bcc6099df8be8a38afdb73
Sofiia13 <sophiyastanish@gmail.com>
2026-03-15 18:51:35 +0200

    start developing design

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-8">Event Aggregator</h1>
+    </div>
+  );
+};
---
+++ frontend/data/events.ts
@@ -0,0 +1,27 @@
+export const mockEvents = [
+  { id: 1, title: "Jazz Night", date: "2026-04-20", location: "Lviv Opera" },
+  {
+    id: 2,
+    title: "IT Meetup",
+    date: "2026-04-25",
+    location: "Lviv IT Cluster",
+  },
+  {
+    id: 3,
+    title: "Tech Conference",
+    date: "2026-05-10",
+    location: "Lviv Convention Center",
+  },
+  {
+    id: 4,
+    title: "Startup Pitch Night",
+    date: "2026-05-13",
+    location: "Lviv Innovation Hub",
+  },
+  {
+    id: 5,
+    title: "Vystavka Vin",
+    date: "2026-04-11",
+    location: "ZAG Lviv",
+  },
+];
